cdb803ddf3f2cf0c5685a0e0490284ec3f270888
cenzhilin <690693258@qq.com>
2020-03-23 22:17:18 +0800

    Updates

---
+++ precache-manifest.1391dd2eb9ad3caf8e43bc7c22cb5711.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2755b12ce6b32a861437865bcc47a513",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "d303234a4f1f1dad2cdc",
+    "url": "/demo/static/css/main.6dbf084e.chunk.css"
+  },
+  {
+    "revision": "92abbfe5ea3c26f2ce11",
+    "url": "/demo/static/js/2.2c5b3f1d.chunk.js"
+  },
+  {
+    "revision": "664340cd0178efc688d8dcce36fc14b9",
+    "url": "/demo/static/js/2.2c5b3f1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d303234a4f1f1dad2cdc",
+    "url": "/demo/static/js/main.f71efa17.chunk.js"
+  },
+  {
+    "revision": "3ee584a1970888ebe32a",
+    "url": "/demo/static/js/runtime-main.1eefdecc.js"
+  },
+  {
+    "revision": "770b72ff79df439879ae34e38f4cd075",
+    "url": "/demo/static/media/bloodPressure.770b72ff.svg"
+  },
+  {
+    "revision": "dbcb9d25709be601a1bf8c91be911d1e",
+    "url": "/demo/static/media/bloodSugar.dbcb9d25.svg"
+  },
+  {
+    "revision": "9d8801f3953d53f01013e136727903f6",
+    "url": "/demo/static/media/bodyFat.9d8801f3.svg"
+  },
+  {
+    "revision": "614b632da707b7e9d5764f0390f4122d",
+    "url": "/demo/static/media/heartOxygen.614b632d.svg"
+  },
+  {
+    "revision": "c22bf753ffe7198801118a7b882c5c25",
+    "url": "/demo/static/media/heartRate.c22bf753.svg"
+  },
+  {
+    "revision": "b544af77735f2af6ab341014791d7eaa",
+    "url": "/demo/static/media/temperature.b544af77.svg"
+  },
+  {
+    "revision": "461192fea48c6beb2781f2292740a0b3",
+    "url": "/demo/static/media/多用户模式.461192fe.svg"
+  },
+  {
+    "revision": "838a2c90df112d88868644b582784ebf",
+    "url": "/demo/static/media/异常.838a2c90.svg"
+  },
+  {
+    "revision": "b47d3c992901917e06439f2797c8e473",
+    "url": "/demo/static/media/新增用户.b47d3c99.svg"
+  },
+  {
+    "revision": "1d6e0cbe68e026d114dc7684a4f2af9c",
+    "url": "/demo/static/media/用户.1d6e0cbe.svg"
+  }
+]);
